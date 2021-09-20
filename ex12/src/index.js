60f3efa6fc35d9d2f7cc51965abf58d22c1ddf74
zigerkw <zejdhadzic@hotmail.com>
2021-09-20 21:35:20 +0300

    dvanaesti zadatak

---
+++ ex12/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+
+function randomInteger() {
+
+Math.floor(Math.random() * 30);
+return Math.random;
+}
+
+randomInteger();
+console.log(Math.floor(Math.random() * 30));
+
+// Only change cod above this line
+module.exports = randomInteger;
+
