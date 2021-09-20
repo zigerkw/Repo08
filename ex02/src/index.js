9bbf4731dc7939287d90ecf94b26b7d541726f39
zigerkw <zejdhadzic@hotmail.com>
2021-09-20 12:16:56 +0300

    prva vjezba

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,7 +1,10 @@
+
+// Write code below this line
 function functionPrintInfo(name, age) {
 
     console.log(name, age);
     
 }
 functionPrintInfo("John", 23);
+// Write code above this line
 module.exports = functionPrintInfo
