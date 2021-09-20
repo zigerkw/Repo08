c13a7d29421c00d45f9f1cd1adfc2ec83daea04e
zigerkw <zejdhadzic@hotmail.com>
2021-09-20 22:11:10 +0300

    cetrnaesta

---
+++ ex14/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function convertStrToInt(str) {
+    return parseInt(str);
+}
+convertStrToInt("44");
+console.log(parseInt(convertStrToInt("44")));
+console.log(parseInt(convertStrToInt("27")));
+console.log(parseInt(convertStrToInt("JeffBezos")));
+
+// Only change code above this line
+module.exports = convertStrToInt;
+
