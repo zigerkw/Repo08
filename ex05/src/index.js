95f4e6310333dc6ace8dd351a230316f112f6eb8
zigerkw <zejdhadzic@hotmail.com>
2021-09-20 14:15:21 +0300

    peta vjezba

---
+++ ex05/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var dress = "little dress";
+
+function myDress() {
+    var dress = "ball gown";
+
+// Only change code above this line
+return dress;    
+}
+// Only change code below this line
+myDress();
+// Only change code above this line
+console.log(dress)
+
+module.exports = myDress;
